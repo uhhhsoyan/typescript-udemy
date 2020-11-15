512e0b29eb6961b66485b7f1118ba9f4d8c2ab7f
Eric Essoyan <eric.essoyan@gmail.com>
2020-11-14 22:58:43 -0800

    first commit

---
+++ features/annotations/functions.ts
@@ -0,0 +1,14 @@
+// arrow function
+const add = (a: number, b: number): number => {
+  return a + b;
+}
+
+// named function
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+// anonymous function assigned to a variable
+const multiply = function(a: number, b: number): number {
+  return a * b;
+}
