cf7fbc962b8abf79d2759e5e7f22c5791440f675
Eric Essoyan <ericessoyan@Erics-MacBook-Pro-2.local>
2020-11-30 10:12:10 -0800

    git cleanup

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,12 @@
 import { User } from './User';
 import { Company } from './Company';
 
-const user = new User();
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
 
-console.log(user);
-
-const company = new Company();
-
-console.log(company);
+google
